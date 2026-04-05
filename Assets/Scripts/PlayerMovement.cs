13d85d579bccdd852fe80c96c4b43fd9c490a3de
milliemangion <amelia.mangion@gmail.com>
2026-04-05 20:29:17 +0200

    Implemented gravity flip mechanic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,16 +3,40 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public float gravityScale = 3f;
 
     private Rigidbody2D rb;
+    private bool isUpsideDown = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = gravityScale;
     }
 
     void Update()
     {
+        // Constant forward movement
         rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
+
+        // Flip gravity on key press
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FlipGravity();
+        }
+    }
+
+    void FlipGravity()
+    {
+        isUpsideDown = !isUpsideDown;
+
+        rb.gravityScale *= -1;
+
+        // Flip player visually
+        transform.localScale = new Vector3(
+            transform.localScale.x,
+            transform.localScale.y * -1,
+            transform.localScale.z
+        );
     }
 }
